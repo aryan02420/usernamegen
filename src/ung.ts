2b522decf69390b86eab80bd5cf2c4aca4dcb3a9
aryan02420 <aryan02420@gmail.com>
2021-04-01 16:19:41 +0530

    generates random usernames

--- src/ung.ts
+++ src/ung.ts
@@ -1,39 +1,55 @@
 import usernameData from "./words";
-import seedrandom from "seedrandom";
+import * as seedrandom from "seedrandom";
+
 
 function clamp(value:number, min:number, max:number):number {
     return Math.min(Math.max(min, Number(value)), max);;
-}
-
+};
 function map(value:number, fromMin:number, fromMax:number, toMin:number, toMax:number, clamping:boolean=false):number {
     if (clamping) value = clamp(value, fromMin, fromMax);
     return (value-fromMin) * (toMax-toMin) / (fromMax-fromMin) + toMin;
-}
+};
 
-function UNG() {
-    this.rng = seedrandom;
-    this.words = usernameData;
-    this.formattor = (...args) => {
-        return args.join('');
-    };
-    this.generateUsername = () => {
-        return new Username(this, ['hello', 'world', '123'], 123);
-    };
-}
 
 class Username {
-    parent: any;
-    fullName: [string, string, string];
+    parent: UNG;
+    fullName: string[];
     sequence: any;
-    constructor(p, fn, seq) {
+    constructor(p: UNG, fn: string[], seq: any) {
         this.parent = p;
         this.fullName = fn;
         this.sequence = seq;
     };
-    toString = (format = this.parent.formattor) => {
+    toString = (format = this.parent.formattor): string => {
         return format(...this.fullName);
+    };
+};
+
+class UNG {
+    constructor(){};
+    rng: any = seedrandom;
+    words = usernameData;
+    formattor = (...args:string[]): string => {
+        return args.join('');
+    };
+    generateUsername = (seed: any = undefined): Username => {
+        const rng: any = this.rng(seed);
+        const adjectiveIndex: number = Math.floor(map(rng(),0,1,0,this.words.adjectives.length,true));
+        const nounIndex: number = Math.floor(map(rng(),0,1,0,this.words.nouns.length,true));
+        const numDigits: number =  Math.floor(map(rng(),0,1,1,6));
+        const firstName: string = this.words.adjectives.list[adjectiveIndex];
+        const lastName: string = this.words.nouns.list[nounIndex];
+        let num: string = "";
+        for (let i = 0; i < numDigits; i++) {
+            const digit = Math.floor(map(rng(),0,1,0,10));
+            num += digit.toString();
+        }        
+        return new Username(this, [firstName, lastName, num], rng);
     };
 }
 
-let ung = new UNG();
-console.log(ung.generateUsername().toString())
+
+let ung = new UNG()
+console.log(ung.generateUsername().toString());
+
+export default UNG;
